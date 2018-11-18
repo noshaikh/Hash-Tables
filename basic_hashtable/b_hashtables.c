b814d7c739b4f37770052df246c518e09ff76292
kaibo <38451683+noshaikh@users.noreply.github.com>
2018-11-18 10:01:06 -0800

    hashtables done

--- basic_hashtable/b_hashtables.c
+++ basic_hashtable/b_hashtables.c
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 #include <string.h>
 
-
 /****
   Basic hash table key/value pair
  ****/
-typedef struct Pair {
+typedef struct Pair
+{
   char *key;
   char *value;
 } Pair;
@@ -14,7 +14,8 @@ typedef struct Pair {
 /****
   Basic hash table
  ****/
-typedef struct BasicHashTable {
+typedef struct BasicHashTable
+{
   int capacity;
   Pair **storage;
 } BasicHashTable;
@@ -36,7 +37,8 @@ Pair *create_pair(char *key, char *value)
  ****/
 void destroy_pair(Pair *pair)
 {
-  if (pair != NULL) free(pair);
+  if (pair != NULL)
+    free(pair);
 }
 
 /****
@@ -48,15 +50,15 @@ unsigned int hash(char *str, int max)
 {
   unsigned long hash = 5381;
   int c;
-  unsigned char * u_str = (unsigned char *)str;
+  unsigned char *u_str = (unsigned char *)str;
 
-  while ((c = *u_str++)) {
+  while ((c = *u_str++))
+  {
     hash = ((hash << 5) + hash) + c;
   }
 
   return hash % max;
 }
-
 
 /****
   Fill this in.
@@ -66,8 +68,9 @@ unsigned int hash(char *str, int max)
  ****/
 BasicHashTable *create_hash_table(int capacity)
 {
-  BasicHashTable *ht;
-
+  BasicHashTable *ht = malloc(sizeof(BasicHashTable));
+  ht->capacity = capacity;
+  ht->storage = calloc(capacity, sizeof(Pair *));
   return ht;
 }
 
@@ -80,7 +83,18 @@ BasicHashTable *create_hash_table(int capacity)
  ****/
 void hash_table_insert(BasicHashTable *ht, char *key, char *value)
 {
+  unsigned int index = hash(key, ht->capacity);
+  Pair *stored_pair = ht->storage[index];
+  if (stored_pair != NULL)
+  {
+    if (strcmp(key, stored_pair->key) != 0)
+    {
+      printf("WARNING: Hash Table Collision");
+    }
+    destroy_pair(stored_pair);
+  }
 
+  ht->storage[index] = create_pair(key, value);
 }
 
 /****
@@ -90,49 +104,85 @@ void hash_table_insert(BasicHashTable *ht, char *key, char *value)
  ****/
 void hash_table_remove(BasicHashTable *ht, char *key)
 {
+  unsigned int index = hash(key, ht->capacity);
+  Pair *stored_pair = ht->storage[index];
 
-}
+  if (stored_pair == NULL)
+  {
+    printf("ERROR: Key does not exist!\n");
+  }
+  else
+  {
+    destroy_pair(stored_pair);
+    ht->storage[index] = NULL;
+  }
 
-/****
+  /****
   Fill this in.
 
   Should return NULL if the key is not found.
  ****/
-char *hash_table_retrieve(BasicHashTable *ht, char *key)
-{
-  return NULL;
-}
+  char *hash_table_retrieve(BasicHashTable * ht, char *key)
+  {
+    unsigned int index = hash(key, ht->capacity);
 
-/****
+    Pair *stored_pair = ht->storage[index];
+
+    if (stored_pair == NULL)
+    {
+      printf("ERROR: Key does not exist!\n");
+    }
+    else
+    {
+      if (strcmp(key, stored_pair->key) == 0)
+      {
+        return stored_pair->value;
+      }
+      else
+      {
+        printf("ERROR: Key does not exist!\n");
+      }
+    }
+  }
+
+  /****
   Fill this in.
 
   Don't forget to free any malloc'ed memory!
  ****/
-void destroy_hash_table(BasicHashTable *ht)
-{
-
-}
-
-
-#ifndef TESTING
-int main(void)
-{
-  struct BasicHashTable *ht = create_hash_table(16);
-
-  hash_table_insert(ht, "line", "Here today...\n");
-
-  printf("%s", hash_table_retrieve(ht, "line"));
-
-  hash_table_remove(ht, "line");
-
-  if (hash_table_retrieve(ht, "line") == NULL) {
-    printf("...gone tomorrow. (success)\n");
-  } else {
-    fprintf(stderr, "ERROR: STILL HERE\n");
+  void destroy_hash_table(BasicHashTable * ht)
+  {
+    //free all storage
+    for (int i = 0; i < ht->capacity; i++)
+    {
+      destroy_pair(ht->storage[i]);
+    }
+    free(ht->storage);
+    free(ht);
   }
 
-  destroy_hash_table(ht);
+#ifndef TESTING
+  int main(void)
+  {
+    struct BasicHashTable *ht = create_hash_table(16);
 
-  return 0;
-}
+    hash_table_insert(ht, "line", "Here today...\n");
+
+    printf("%s", hash_table_retrieve(ht, "line"));
+
+    hash_table_remove(ht, "line");
+
+    if (hash_table_retrieve(ht, "line") == NULL)
+    {
+      printf("...gone tomorrow. (success)\n");
+    }
+    else
+    {
+      fprintf(stderr, "ERROR: STILL HERE\n");
+    }
+
+    destroy_hash_table(ht);
+
+    return 0;
+  }
 #endif
